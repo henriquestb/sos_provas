a3dd38db86d21e856679d3e0d2b9d2a564184a54
Henrique Starling <henrique_st@yahoo.com.br>
2020-05-27 18:39:51 -0300

    fotter + cards das subjets

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,70 @@
+class CoursesController < ApplicationController
+  before_action :set_course, only: [:show, :edit, :update, :destroy]
+
+  def home
+  end
+
+  def index
+    @courses = Course.all
+  end
+
+  def show
+    @subjects = @course.subjects
+  end
+
+  # GET /courses/new
+  def new
+    @course = course.new
+  end
+
+  # GET /courses/1/edit
+  def edit
+    # @dose = Dose.new
+  end
+
+  def destroy
+    @course.destroy
+    respond_to do |format|
+      format.html { redirect_to courses_url, notice: 'course was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @course.update(course_params)
+        format.html { redirect_to @course, notice: 'course was successfully updated.' }
+        format.json { render :show, status: :ok, location: @course }
+      else
+        format.html { render :edit }
+        format.json { render json: @course.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def create
+    @course = course.new(course_params)
+
+    respond_to do |format|
+      if @course.save
+        format.html { redirect_to @course, notice: 'course was successfully created.' }
+        format.json { render :show, status: :created, location: @course }
+      else
+        format.html { render :new }
+        format.json { render json: @course.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+  def set_course
+    @course = course.find(params[:id])
+  end
+  # Only allow a list of trusted parameters through.
+  def course_params
+    params.require(:course).permit(:name)
+  end
+end
+
+end
---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,68 @@
+class SubjectsController < ApplicationController
+  before_action :set_subject, only: [:show, :edit, :update, :destroy]
+
+  def home
+  end
+
+  def index
+    @subjects = Subject.all
+  end
+
+  def show
+    @doses = @subject.doses
+  end
+
+  # GET /subjects/new
+  def new
+    @subject = Subject.new
+  end
+
+  # GET /subjects/1/edit
+  def edit
+    @dose = Dose.new
+  end
+
+  def destroy
+    @subject.destroy
+    respond_to do |format|
+      format.html { redirect_to subjects_url, notice: 'subject was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @subject.update(subject_params)
+        format.html { redirect_to @subject, notice: 'subject was successfully updated.' }
+        format.json { render :show, status: :ok, location: @subject }
+      else
+        format.html { render :edit }
+        format.json { render json: @subject.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def create
+    @subject = subject.new(subject_params)
+
+    respond_to do |format|
+      if @subject.save
+        format.html { redirect_to @subject, notice: 'subject was successfully created.' }
+        format.json { render :show, status: :created, location: @subject }
+      else
+        format.html { render :new }
+        format.json { render json: @subject.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+  def set_subject
+    @subject = subject.find(params[:id])
+  end
+  # Only allow a list of trusted parameters through.
+  def subject_params
+    params.require(:subject).permit(:name)
+  end
+end
