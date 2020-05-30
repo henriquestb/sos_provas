b6f07d82876b8af4ee78a6ff45037997994ee6b3
Henrique Starling <henrique_st@yahoo.com.br>
2020-05-30 19:30:51 -0300

    lets go

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
   root to: 'pages#home'
   resources :courses do
-    resources :subjects, only: [:show]
+    resources :subjects, only: [:show, :index]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
