91ef1846c0cda6a6b86a7f479927f52720ee55af
Henrique Starling <henrique_st@yahoo.com.br>
2020-06-06 18:31:04 -0300

    adicionando javascript na ultima pagina

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -5,8 +5,8 @@ module VideosHelper
   VIMEO_REGEX = %r(^https?:\/\/(?:.*?)\.?(vimeo)\.com\/(\d+).*$)
 
   # Get Vimeo video iframe from given URL
-  def get_vimeo_iframe id, width, height
-    uri      = "https://vimeo.com/api/oembed.json?url=https%3A//vimeo.com/#{ id }&width=#{ width }&height=#{ height }"
+  def get_vimeo_iframe id
+    uri      = "https://vimeo.com/api/oembed.json?url=https%3A//vimeo.com/#{ id }"
     response = Net::HTTP.get( URI.parse( uri ))
     json     = JSON.parse response
     json['html'].html_safe
