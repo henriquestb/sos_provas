316065da85c3f4a24d41734e260761d9515bb4b0
Henrique Starling <henrique_st@yahoo.com.br>
2020-06-16 19:59:56 -0300

    espero que seja o ultimo

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -2,6 +2,6 @@ class StripeCheckoutSessionService
   def call(event)
     order = Order.find_by(checkout_session_id: event.data.object.id)
     order.update(state: 'paid')
-    BoughtSubject.create!(subject: order.subject, user: current_user)
+    BoughtSubject.create!(subject: order.subject, user: order.user)
   end
 end
