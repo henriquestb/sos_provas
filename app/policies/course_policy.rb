45d3e9401e09d17d6f45e03ffb3b0401a524776f
Bruno Toledo <brunogalizzi@gmail.com>
2020-06-17 23:21:57 -0300

    Form to create/update/destroy

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -1,4 +1,6 @@
 class CoursePolicy < ApplicationPolicy
+  attr_reader :user, :record
+
   class Scope < Scope
     def resolve
       scope.all
@@ -6,6 +8,18 @@ class CoursePolicy < ApplicationPolicy
   end
 
   def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    true
+  end
+
+  def destroy?
     true
   end
 
